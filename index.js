4a4488f4b5a3dee359d08816b4f15874d3c0d9a0
reggi <thomas@reggi.com>
2015-06-29 20:00:23 -0400

    working example fixes #1

--- index.js
+++ index.js
@@ -7,16 +7,21 @@ var oauth2 = new OAuth2(process.env.STITCH_CLIENT_ID,
                         process.env.STITCH_CLIENT_SECRET,
                         "https://api-pub.stitchlabs.com/",
                         "authorize",
-                        'oauth2/token',
+                        'oauth/token',
                         null)
 
 /*
-Builds the url:
+Builds the url
 https://api-pub.stitchlabs.com/authorize?redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fredirect&client_id={{ MY_CLIENT }}
 */
 
+var oauthState = "kitten"
+
 var authURL = oauth2.getAuthorizeUrl({
-    redirect_uri: 'http://localhost:3000/redirect'
+    redirect_uri: 'http://localhost:3000/redirect',
+    scope: [],
+    response_type: "code",
+    state: oauthState
 })
 
 app.get('/', function(req, res){
@@ -24,7 +29,13 @@ app.get('/', function(req, res){
 })
 
 app.get('/redirect', function(req, res){
-  return res.json(req.query)
+  return oauth2.getOAuthAccessToken(req.query.code, {
+    "grant_type": "authorization_code",
+    "redirect_uri": 'http://localhost:3000/redirect',
+  }, function (e, access_token, refresh_token, results){
+    console.log(access_token)
+    return res.json(access_token)
+  })
 })
 
 app.listen(3000)
